9c281707c10379968ab8036337de910270cd4fa0
Hao-Ting Wang <htwangtw@gmail.com>
2020-03-12 11:07:10 +0000

    check fsf and feat exisits

--- code/src/batch_first_level.sh
+++ code/src/batch_first_level.sh
@@ -12,21 +12,31 @@ mkdir -p ${OUTPUT}/logs
 cd $OUTPUT
 for SUBJ in ${SUBJ_LIST}; do
   # create fsf template
-  echo "sub-${SUBJ}: create .fsf"
-  mkdir -p ${OUTPUT}/sub-${SUBJ}
-  
-  path=~/projects/critchley_depersonalisation/data/derivatives/func_smooth-6mm/sub-${SUBJ}/func/*.gz
-  N_VOL=$(fslinfo $path | grep dim4 | cut -d " " -f12)
-  TR=$(fslinfo $path | grep pixdim4 | awk '{print $2}')
-  for i in $TEMPLATE; do
-    sed -e 's@SUBJECT@'$SUBJ'@g' \
-        -e 's@VOLUMENUMBER@'$N_VOL'@g' \
-        -e 's@TR@'$TR'@g' \
-        -e 's@OUTPUT@'$OUTPUT'@g' \
-        <$i> ${OUTPUT}/sub-${SUBJ}/sub-${SUBJ}_level_1.fsf
-  done
-  cd ${OUTPUT}/logs
+  fsf=${OUTPUT}/sub-${SUBJ}/sub-${SUBJ}_level_1.fsf
+  if [[ -f $fsf ]]
+  then
+    echo "fsf file exist!"
+  else
+    echo "sub-${SUBJ}: create .fsf"
+    mkdir -p ${OUTPUT}/sub-${SUBJ}
+   
+    path=~/projects/critchley_depersonalisation/data/derivatives/func_smooth-6mm/sub-${SUBJ}/func/*.gz
+    N_VOL=$(fslinfo $path | grep dim4 | cut -d " " -f12)
+    TR=$(fslinfo $path | grep pixdim4 | awk '{print $2}')
+    for i in $TEMPLATE; do
+      sed -e 's@SUBJECT@'$SUBJ'@g' \
+          -e 's@VOLUMENUMBER@'$N_VOL'@g' \
+          -e 's@TR@'$TR'@g' \
+          -e 's@OUTPUT@'$OUTPUT'@g' \
+          <$i> $fsf
+    done
+  fi
   # send the preprocess job to cluster
-  fsl_sub -N sub-${SUBJ} -l ${OUTPUT}/logs \
-          feat ${OUTPUT}/sub-${SUBJ}/sub-${SUBJ}_level_1.fsf
+  FEAT_DIR=${OUTPUT}/sub-${SUBJ}/sub-${SUBJ}.feat
+  if [[ ! -d "$FEAT_DIR" ]]; then
+    fsl_sub -N sub-${SUBJ} -l ${OUTPUT}/logs feat ${OUTPUT}/sub-${SUBJ}/sub-${SUBJ}_level_1.fsf
+    echo "run feat"
+  else
+    echo "Feat directory exist"
+  fi
 done
