38336ca5f57b16bc8168e25e7e10e98a9f3b13a5
Hao-Ting Wang <htwangtw@gmail.com>
2020-03-20 09:10:33 +0000

    new FSL analysis

--- code/src/batch_first_level.sh
+++ code/src/batch_first_level.sh
@@ -5,9 +5,10 @@
 SUBJ_LIST=$( sed -n -E "s/sub-(\S*)\>.*/\1/gp" \
              ~/projects/critchley_depersonalisation/code/participants.tsv )
 
-TEMPLATE=/research/cisc2/projects/critchley_depersonalisation/code/templates/$1
-OUTPUT=/research/cisc1/projects/critchley_depersonalisation/$2
-
+# TEMPLATE=/research/cisc2/projects/critchley_depersonalisation/code/templates/$1
+# OUTPUT=/research/cisc1/projects/critchley_depersonalisation/$2
+TEMPLATE=/home/hw1012/projects/critchley_depersonalisation/code/templates/HRV_lvl1.fsf
+OUTPUT=/home/hw1012/projects/critchley_depersonalisation/scratch/FSL_HRV_no_BPM
 mkdir -p ${OUTPUT}/logs
 cd $OUTPUT
 for SUBJ in ${SUBJ_LIST}; do
@@ -21,8 +22,9 @@ for SUBJ in ${SUBJ_LIST}; do
     mkdir -p ${OUTPUT}/sub-${SUBJ}
    
     path=~/projects/critchley_depersonalisation/data/derivatives/func_smooth-6mm/sub-${SUBJ}/func/*.gz
-    N_VOL=$(fslinfo $path | grep dim4 | cut -d " " -f12)
+    N_VOL=$(fslinfo $path | grep ^dim4 | awk '{print $2}')
     TR=$(fslinfo $path | grep pixdim4 | awk '{print $2}')
+    echo $N_VOL
     for i in $TEMPLATE; do
       sed -e 's@SUBJECT@'$SUBJ'@g' \
           -e 's@VOLUMENUMBER@'$N_VOL'@g' \
@@ -34,7 +36,7 @@ for SUBJ in ${SUBJ_LIST}; do
   # send the preprocess job to cluster
   FEAT_DIR=${OUTPUT}/sub-${SUBJ}/sub-${SUBJ}.feat
   if [[ ! -d "$FEAT_DIR" ]]; then
-    fsl_sub -N sub-${SUBJ} -l ${OUTPUT}/logs feat ${OUTPUT}/sub-${SUBJ}/sub-${SUBJ}_level_1.fsf
+    feat ${OUTPUT}/sub-${SUBJ}/sub-${SUBJ}_level_1.fsf
     echo "run feat"
   else
     echo "Feat directory exist"
