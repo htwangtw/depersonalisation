6c615f9f7cc3c2efb886938931050ab4c0cf5dff
Hao-Ting Wang <htwangtw@gmail.com>
2020-04-15 09:47:59 +0100

    group level analysis

--- code/grouplevel_cluster.sh
+++ code/grouplevel_cluster.sh
@@ -2,8 +2,8 @@
 #$ -N fsl_group
 #$ -o /home/$USER/logs
 #$ -j y
-#$-t 1-6
-#$-tc 6
+#$-t 1-10
+#$-tc 1
 
 . ${HOME}/.bash_profile
 SEED_DIR=${HOME}/projects/critchley_depersonalisation/code/ppi_seed
