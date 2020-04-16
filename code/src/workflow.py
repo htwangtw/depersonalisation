34359d7d75b961eec6e7d9acef54da8ff1e5a9c2
Hao-Ting Wang <htwangtw@gmail.com>
2020-04-16 12:18:39 +0100

    adapt new interface

--- code/src/workflow.py
+++ code/src/workflow.py
@@ -250,38 +250,31 @@ def group_randomise_wf(input_dir, output_dir, subject_list,
         randomise.inputs.tfce = True
         # randomise.inputs.demean = True
 
-        onesampleT_randomise = pe.Node(fsl.Randomise(),
-                                       name="onesampleT_randomise")
-        onesampleT_randomise.inputs.num_perm = 1000
-        onesampleT_randomise.inputs.vox_p_values = True
-        onesampleT_randomise.inputs.tfce = True
-        # onesampleT_randomise.inputs.demean = True
-        onesampleT_randomise.inputs.one_sample_group_mean = True
-        # Create DataSink object
-        gsinker = pe.Node(DataSink(), name=f'sinker_{contrast}_group')
-        gsinker.inputs.base_directory = output_dir + os.sep + analysis_name
-        gsinker.inputs.substitutions = [('tstat1', 'tstat'),
-                                        ('randomise', 'fullsample')]
         # Create DataSink object
         sinker = pe.Node(DataSink(), name=f'sinker_{contrast}')
         sinker.inputs.base_directory = output_dir + os.sep + analysis_name
         sinker.inputs.substitutions = [
             ('randomise_tfce_corrp_tstat1',
-             'control_tfce_corrp_tstat'),
+             'fullsample_tfce_corrp_tstat'),
             ('randomise_tfce_corrp_tstat2',
-             'patient_tfce_corrp_tstat'),
+             'control_tfce_corrp_tstat'),
             ('randomise_tfce_corrp_tstat3',
-             'patient_wrt_control_tfce_corrp_tstat'),
+             'patient_tfce_corrp_tstat'),
             ('randomise_tfce_corrp_tstat4',
+             'patient_wrt_control_tfce_corrp_tstat'),
+            ('randomise_tfce_corrp_tstat5',
              'control_wrt_patients_tfce_corrp_tstat'),
             ('randomise_tstat1',
-             'control_tstat'),
+             'fullsample_tstat'),
             ('randomise_tstat2',
-             'patient_tstat'),
+             'control_tstat'),
             ('randomise_tstat3',
-             'patient_wrt_control_tstat'),
+             'patient_tstat'),
             ('randomise_tstat4',
+             'patient_wrt_control_tstat'),
+            ('randomise_tstat5',
              'control_wrt_patients_tstat')]
+
         wk.connect([
             (file_grabber, concat_copes, [("cope_file", "cope_file")]),
             (concat_copes, randomise, [("output_dir", "in_file")]),
@@ -293,13 +286,6 @@ def group_randomise_wf(input_dir, output_dir, subject_list,
                  f'contrast_{contrast}.@tstat_files'),
                 ('t_corrected_p_files',
                  f'contrast_{contrast}.@t_corrected_p_files')]),
-            (concat_copes, onesampleT_randomise, [("output_dir", "in_file")]),
-            (prep_files, onesampleT_randomise, [("outputnode.mask", "mask")]),
-            (onesampleT_randomise, gsinker, [
-                ('tstat_files',
-                 f'contrast_{contrast}.@group_tstat_files'),
-                ('t_corrected_p_files',
-                 f'contrast_{contrast}.@group_t_corrected_p_files')]),
             ])
         meta_workflow.add_nodes([wk])
     return meta_workflow
