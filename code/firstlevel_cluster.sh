ae9bc82ea8c1cbd070fd3a106bdc2adc0b96ee38
Hao-Ting Wang <htwangtw@gmail.com>
2020-04-20 14:18:16 +0100

    cluster job

--- code/firstlevel_cluster.sh
+++ code/firstlevel_cluster.sh
@@ -17,17 +17,17 @@ for subj in $SUBJ_LIST; do
 echo sub-$subj
 
 # generate nuisance and task regressors
-bash ./create_regressors.sh ${subj}
+# bash ./create_regressors.sh ${subj}
 
 # task only
 ./first_level.sh heart_wrt_note_level_1.fsf FSL_task ${subj}
 # ./registration.sh FSL_task ${subj}
-done
+
 #PPI
-for seed in $(ls ${SEED_DIR}/*); do
-  SEED_NAME=$(echo $(basename $seed) | cut -d - -f4 | cut -d . -f1)
-  echo sub-${subj}_${SEED_NAME}
-  bash ./first_level_PPI.sh PPI_level1.fsf FSL_PPI-${SEED_NAME} ${subj} ${seed}  
-done
+# for seed in $(ls ${SEED_DIR}/*); do
+#   SEED_NAME=$(echo $(basename $seed) | cut -d - -f4 | cut -d . -f1)
+#   echo sub-${subj}_${SEED_NAME}
+#   bash ./first_level_PPI.sh PPI_level1.fsf FSL_PPI-${SEED_NAME} ${subj} ${seed}  
+# done
 
 done
